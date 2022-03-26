8b048eb057025b2c82291b818f65620e06be2fe2
EdgarFalco <56967437+EdgarFalco@users.noreply.github.com>
2022-03-26 16:10:04 +0100

    register logic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../service/auth/login.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  user_register: any = {
+    username: '',
+    nombre: '',
+    apellidos: '',
+    email: '',
+    password: '',
+    foto_perfil: null,
+    fecha_creacion: null,
+    fecha_modificacion: null,
+    role: null,
+    usuario_steam: null,
+    grupos: null,
+  };
 
-  constructor() { }
+  isSuccessful = false;
 
-  ngOnInit(): void {
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit(): void {}
+
+  newUser(): void {
+    this.isSuccessful = false;
+    const data = {
+      username: this.user_register.username,
+      nombre: this.user_register.nombre,
+      apellidos: this.user_register.apellidos,
+      email: this.user_register.password,
+      password: this.user_register.password,
+      foto_perfil: this.user_register.foto_perfil,
+      fecha_creacion: this.user_register.fecha_creacion,
+      fecha_modificacion: this.user_register.fecha_modificacion,
+      role: this.user_register.role,
+      usuario_steam: this.user_register.usuario_steam,
+      grupos: this.user_register.grupos,
+    };
+
+    this.loginService.add(data).subscribe(
+      (response) => {
+        console.log(response);
+        this.isSuccessful = true;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
