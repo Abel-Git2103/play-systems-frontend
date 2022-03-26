2adfbe5481fe00c1b098808952666dcebc777b34
Abel-Git2103 <abel_jornet@hotmail.com>
2022-03-26 16:53:43 +0100

    Models

--- src/app/group-creation/group-creation.component.ts
+++ src/app/group-creation/group-creation.component.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Group } from '../models/group.model';
 import { GroupService } from '../service/group.service';
 
 @Component({
   selector: 'app-group-creation',
   templateUrl: './group-creation.component.html',
-  styleUrls: ['./group-creation.component.css']
+  styleUrls: ['./group-creation.component.css'],
 })
 export class GroupCreationComponent implements OnInit {
+  grupo: Group = {};
 
-  groups: any = null;
+  submitted = false;
 
-  constructor(private groupService: GroupService) {}
+  constructor(private groupService: GroupService, private router: Router) {}
 
-  ngOnInit() {
-    this.groupService
-      .retornar()
-      .subscribe((result) => (this.groups = result));
+  ngOnInit(): void {}
+
+  saveGroup(): void {
+    const data = {
+      nombre_grupo: this.grupo.nombre_grupo,
+      descripcion: this.grupo.descripcion,
+      juego: this.grupo.juego,
+    };
+
+    this.groupService.add(data).subscribe(
+      (response) => {
+        console.log(response);
+        this.submitted = true;
+        this.router.navigate(['/chat']);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
---
+++ src/app/models/group.model.ts
@@ -0,0 +1,6 @@
+export class Group {
+  id?: any;
+  nombre_grupo?: string;
+  juego?: any;
+  descripcion?: string;
+}
