8923a4e208cb73c6bd505af082f1d2eb2588993f
EdgarFalco <56967437+EdgarFalco@users.noreply.github.com>
2022-03-26 18:31:28 +0100

    pruebas

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
 import { LoginService } from '../service/auth/login.service';
 
 @Component({
@@ -7,18 +8,18 @@ import { LoginService } from '../service/auth/login.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  user_register: any = {
-    username: '',
-    nombre: '',
-    apellidos: '',
-    email: '',
-    password: '',
-    foto_perfil: '',
+  user_register: User = {
+    apellidos: null,
+    email: null,
     fecha_creacion: '',
     fecha_modificacion: '',
-    role: '',
+    foto_perfil: '',
+    grupos: '1',
+    nombre: null,
+    password: null,
+    role: 'ROLE_USER',
+    username: null,
     usuario_steam: '',
-    grupos: null,
   };
 
   isSuccessful = false;
@@ -29,26 +30,28 @@ export class RegisterComponent implements OnInit {
 
   newUser(): void {
     const data = {
-      username: this.user_register.username,
-      nombre: this.user_register.nombre,
       apellidos: this.user_register.apellidos,
       email: this.user_register.password,
-      password: this.user_register.password,
-      foto_perfil: this.user_register.foto_perfil,
       fecha_creacion: this.user_register.fecha_creacion,
       fecha_modificacion: this.user_register.fecha_modificacion,
-      role: this.user_register.role,
-      usuario_steam: this.user_register.usuario_steam,
+      foto_perfil: this.user_register.foto_perfil,
       grupos: this.user_register.grupos,
+      nombre: this.user_register.nombre,
+      password: this.user_register.password,
+      role: this.user_register.role,
+      username: this.user_register.username,
+      usuario_steam: this.user_register.usuario_steam,
     };
 
     this.loginService.add(data).subscribe(
       (response) => {
         console.log(response);
         this.isSuccessful = true;
+        alert('Usuario creado correctamente');
       },
       (error) => {
         console.log(error);
+        alert('No se ha podido crear el usuario');
       }
     );
   }
