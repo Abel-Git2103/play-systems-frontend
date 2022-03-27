b2261aec12efbf4b87201d4880db510ca65cb0c2
AlbertNoMes <albertnotariomestres@gmail.com>
2022-03-27 19:04:30 +0200

    profile con errores

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,19 +4,44 @@ import { UserService } from '../service/auth/user.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-
   username: any;
+  usuario: any = {
+    nombre:'',
+    apellidos: '',
+    email: ''
+  };
+
   submitted = false;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.username=window.sessionStorage.getItem("auth-username");
-    this.userService.getById(this.username).subscribe(
+    this.username = window.sessionStorage.getItem('auth-username');
+    this.userService.getById(this.username).subscribe(result=>this.usuario=result)
+  }
+
+  updateUser(nombre: string, apellidos: string, email: string): void {
+
+    usuario: any = {
+      nombre:'',
+      apellidos: '',
+      email: ''
+    };
+
+    const data = {
+      apellidos: this.usuario.apellidos,
+      email: this.usuario.email,
+      nombre: this.usuario.nombre,
+    };
+
+    this.userService.update(this.usuario.username, data).subscribe(
       (response) => {
+        this.usuario.nombre = nombre;
+        this.usuario.apellidos = apellidos;
+        this.usuario.email = email;
         console.log(response);
         this.submitted = true;
       },
@@ -25,5 +50,4 @@ export class ProfileComponent implements OnInit {
       }
     );
   }
-
 }
