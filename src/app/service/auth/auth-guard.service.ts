12066886d2aff0b65db825e792ddd57917c441c6
EdgarFalco <56967437+EdgarFalco@users.noreply.github.com>
2022-03-28 08:57:27 +0200

    auth-guard

--- src/app/service/auth/auth-guard.service.ts
+++ src/app/service/auth/auth-guard.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  canActivate() {
+    const username = window.sessionStorage.getItem('auth-username');
+    const role = window.sessionStorage.getItem('auth-role');
+    console.log(username);
+    console.log(role);
+
+    if (!username) {
+      console.log('usuario logged out');
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+    return true;
+  }
 }
