59eeaa7c841b06681a87951695030794996d6210
EdgarFalco <56967437+EdgarFalco@users.noreply.github.com>
2022-03-30 16:51:50 +0200

    chat observable

--- src/app/my-groups/chat/chat.component.ts
+++ src/app/my-groups/chat/chat.component.ts
@@ -1,26 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ChatService } from 'src/app/service/chat.service';
 import { MessageService } from 'src/app/service/message.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
-export class ChatComponent implements OnInit {
-
+export class ChatComponent implements OnInit, OnDestroy {
   id: any = null;
   mensajes: any = null;
 
-  constructor(private messageService: MessageService, private _route: ActivatedRoute, private router: Router) { }
+  suscription = new Subscription();
+
+  constructor(
+    private messageService: MessageService,
+    private _route: ActivatedRoute,
+    private router: Router
+  ) {}
+  //Implementamos el Ondestroy para liberar memoria
+  ngOnDestroy(): void {
+    console.log('Observable cerrado');
+    this.suscription.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.id = this._route.snapshot.paramMap.get('id');
     console.log(this.id);
 
-    this.messageService.getMessagesByChatId(this.id)
-    .subscribe(
+    this.messageService.getMessagesByChatId(this.id).subscribe(
       (response) => {
         console.log(response);
         this.mensajes = response;
@@ -29,6 +39,18 @@ export class ChatComponent implements OnInit {
         console.log(error);
       }
     );
-  }
 
+    //Cada vez que se añadan mensajes se hara un next i ejecutara este codigo
+    this.suscription = this.messageService.refresh$.subscribe(() => {
+      this.messageService.getMessagesByChatId(this.id).subscribe(
+        (response) => {
+          console.log(response);
+          this.mensajes = response;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    });
+  }
 }
--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -1,21 +1,35 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
+  //Guardara valor de los datos que vayamos añadiendo
+  private _refresh$ = new Subject<void>();
+
   constructor(private http: HttpClient) {}
 
-  getMessagesByChatId(id_chat: any) {
-    return this.http.get(`https://play-systems.herokuapp.com/api/mensajes/chat/${id_chat}`);
+  //Obtendra el valor de los datos añadidos
+  get refresh$() {
+    return this._refresh$;
   }
 
-  add(data: any) {
-    return this.http.post(
-      `https://play-systems.herokuapp.com/api/mensajes`,
-      data
+  getMessagesByChatId(id_chat: any) {
+    return this.http.get(
+      `https://play-systems.herokuapp.com/api/mensajes/chat/${id_chat}`
     );
   }
 
+  add(data: any) {
+    return this.http
+      .post(`https://play-systems.herokuapp.com/api/mensajes`, data)
+      .pipe(
+        //cada vez que se ejecute guardara los datos que lanza
+        tap(() => {
+          this._refresh$.next();
+        })
+      );
+  }
 }
